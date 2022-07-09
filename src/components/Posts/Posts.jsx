edcad9f1a2e6c5e9666bb43ba56ba606c056a5a2
bloon1k <goncharov.vadn@gmail.com>
2022-07-09 22:04:34 +0300

    add background, add posts section, shadows improvements

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Posts-styles.scss'
+
+const Posts = () => {
+    return (
+        <section className={'posts'}>
+            <div className="posts__header">
+                <h2>Latest posts</h2>
+                <button>
+                    Suggest a post
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default Posts;
