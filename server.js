25fd2b8255d469e26bd2261f2fd4fdc9d2946fa9
bloon1k <goncharov.vadn@gmail.com>
2022-07-12 23:33:19 +0300

    add posts, improved adaptive

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 const http = require('http');
 
-const html = `<!DOCTYPE html>
-<html lang="eng">
-    <head>
-    <meta http-equiv = "Content-Type" content = "text/html;charset=UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
-</html>`
+const server = http.createServer();
 
-http.createServer((req, res) => {
-    switch (req.url) {
-        case '/': {
-            res.writeHead(200, {'Content-type': 'text/html'})
-            res.end(html)
-            break
-        }
-        case '/penis': {
-            res.writeHead(200, {'Content-type': 'text/plain'})
-            res.end('sum txt')
-        }
-    }
-}).listen(3001, () => console.log('server is working'))
+server.on('request', (req, res) => {
+    res.end('sum response')
+});
+
+server.listen(3002, 'localhost', () => console.log('server is working'))
